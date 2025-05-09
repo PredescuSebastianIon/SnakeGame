8b18b0a310064e78ba7b6de5d9e8cdf4ec766225
PredescuSebastianIon <sebypredescu@gmail.com>
2025-05-09 13:23:18 +0300

    Now, the snake is running
    Signed-off-by: PredescuSebastianIon <sebypredescu@gmail.com>

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -52,18 +52,64 @@ public class GamePanel extends JPanel implements ActionListener {
 		}
 		g.setColor(Color.red);
 		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+		for (int i = 0; i < bodyParts; i++) {
+			// painting head of snake in a different color (White)
+			// Otherwise, paint the body of snake in green
+			if (i == 0)
+				g.setColor(Color.white);
+			else
+				g.setColor(Color.green);
+			g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+		}
 	}
 	public void newApple() {
 		appleX = random.nextInt((int)(SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
 		appleY = random.nextInt((int)(SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
 	}
 	public void move() {
-
+		// Move snake
+		for (int i = bodyParts - 1; i > 0; i--) {
+			x[i] = x[i - 1];
+			y[i] = y[i - 1];
+		}
+		// Direction of snake's head
+		if (direction == 'R') {
+			x[0] = x[0] + UNIT_SIZE;
+		} else if (direction == 'L') {
+			x[0] = x[0] - UNIT_SIZE;
+		} else if (direction == 'U') {
+			y[0] = y[0] - UNIT_SIZE;
+		} else if (direction == 'D') {
+			y[0] = y[0] + UNIT_SIZE;
+		}
 	}
 	public void checkApple() {
 
 	}
 	public void checkCollisions() {
+		// Check if the head collides with the body
+		for (int i = bodyParts - 1; i > 0; i--) {
+			if (x[0] == x[i] && y[0] == y[i]) {
+				running = false;
+			}
+		}
+		// check if head touches some borders
+		// left border
+		if (x[0] < 0)
+			running = false;
+		// right border
+		if (x[0] > SCREEN_WIDTH)
+			running = false;
+		// top border
+		if (y[0] < 0)
+			running = false;
+		// botton border
+		if (y[0] > SCREEN_HEIGHT)
+			running = false;
+
+		if (!running)
+			timer.stop();
 
 	}
 	public void gameOver(Graphics g) {
@@ -72,12 +118,42 @@ public class GamePanel extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-
+		if (running) {
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
 	}
 
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
+			switch(e.getKeyCode()) {
+				// Case left arrow
+				case KeyEvent.VK_LEFT:
+					if (direction != 'R')
+						direction = 'L';
+					break;
+
+				// Case right arrow
+				case KeyEvent.VK_RIGHT:
+					if (direction != 'L')
+						direction = 'R';
+					break;
+
+				// Case up arrow
+				case KeyEvent.VK_UP:
+					if (direction != 'D')
+						direction = 'U';
+					break;
+
+				// Case down arrow
+				case KeyEvent.VK_DOWN:
+					if (direction != 'U')
+						direction = 'D';
+					break;
+			}
 
 		}
 	}
